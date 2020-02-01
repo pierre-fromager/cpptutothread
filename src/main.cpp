7f40716099f87ad4831c6ce1a42f03918ac77231
Pierre Fromager <pierre@debnovo.pier-infor.fr>
2020-02-01 18:31:25 +0100

    doc/cpptutothread : comments wording

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,7 @@
  * @file main.cpp
  * @author pierre fromager (info@pier-infor.fr)
  * 
- * @brief introduction aux threads en c++11 (// programming)
+ * @brief introduction aux threads avec objets en c++11 (// programming)
  * 
  * Objectifs :
  * + Utiliser 2 objets : un additionneur et un multiplicateur
@@ -12,6 +12,9 @@
  * Moyens :
  * + 2 pools de threads avec chacun 2 thread / pool
  * + 2 exécution de pool distinct
+ * 
+ * Vérifications :
+ * + lancer le programme plusieurs fois et comparer le déroulement
  * 
  * @version 0.1
  * @date 2020-02-01
@@ -28,8 +31,8 @@
 
 using namespace tutothreads;
 
-typedef std::thread Thread;
-typedef std::vector<Thread> ThreadPool;
+typedef typename std::thread Thread;
+typedef typename std::vector<Thread> ThreadPool;
 
 // thread initiale programme principal
 int main()
@@ -44,21 +47,20 @@ int main()
   Padd a(v, msg); // Additioneur utilise v + msg en référence
   Pmul m(v, msg); // Multiplicateur utilise v + msg en référence
 
-  // définition pool pour les tâches des opérations commutatives
+  // définition pool pour tâches d'opérations commutatives
   ThreadPool pnc = {};
-  pnc.push_back(std::thread(&Padd::task, &a, 10)); // ajouter 10
-  pnc.push_back(std::thread(&Padd::task, &a, -3)); // soustrait 3
-  for (std::thread &tnc : pnc)
+  pnc.push_back(Thread(&Padd::task, &a, 10)); // +10
+  pnc.push_back(Thread(&Padd::task, &a, -3)); // -3
+  for (Thread &tnc : pnc)
     tnc.join();
 
-  // définition pool pour les tâches de traitement des opérations non commutatives
+  // définition pool pour tâches d'opérations non commutatives
   ThreadPool pco = {};
-  pco.push_back(std::thread(&Pmul::task, &m, 10)); // multiplier x10
-  pco.push_back(std::thread(&Pmul::task, &m, .5)); // diviser x2
-  for (std::thread &tco : pco)
+  pco.push_back(Thread(&Pmul::task, &m, 10)); // *10
+  pco.push_back(Thread(&Pmul::task, &m, .5)); // /2
+  for (Thread &tco : pco)
     tco.join();
 
-  // le résultat est toujours 40
   msg.add("Valeur finale " + std::to_string(v));
   msg.display();
   assert(v == expected);
